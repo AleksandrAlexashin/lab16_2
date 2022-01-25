784509bf653fa99838dda705129cd9b41b027bee
AleksandrAlexashin <sashokmetall@mail.ru>
2022-01-26 00:03:21 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace lab16_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = string.Empty;
+            using (StreamReader sr = new StreamReader("../../Products.json"))
+            {
+                 jsonString = sr.ReadToEnd();
+            }
+            Tovar[] tovars = JsonSerializer.Deserialize<Tovar[]>(jsonString);
+            Tovar maxTovar = tovars[0];
+            foreach(Tovar t in tovars)
+            {
+                if(t.Price>maxTovar.Price)
+                { maxTovar = t; }
+            }
+            Console.WriteLine($"{maxTovar.Kod} {maxTovar.Name} {maxTovar.Price}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tovar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace lab16_2
+{
+    class Tovar
+    {
+        public int Kod { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
